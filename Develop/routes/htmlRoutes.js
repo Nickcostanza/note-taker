664bb3f6c86d4e082edf21c57abf15158a26748e
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-08-27 14:37:10 -0700

    htmlroute

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,18 @@
+//Dependencies
+
+const path = require('path');
+const router = require('express').Router();
+
+// join path from /notes to notes.html
+
+router.get('/notes', (req, res), => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+// Join path from * to index.hmtl
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+});
+
+module.exports = router;
