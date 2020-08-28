e86e1adb1839539f6e597d2a2624c5591efedc9e
Nick Costanza <nicholas.g.costanza@gmail.com>
2020-08-28 12:03:39 -0700

    started class for storing and displaying notes

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -6,3 +6,27 @@ var { v1: uuidv1 } = require('uuid');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
+class Store {
+    read(){
+        return readFileAsync('db/db.json', 'utf8')
+    }
+    write(note) {
+        return writeFileAsync('dn/db.json', JSON.stringify(note))
+    }
+
+    getNotes() {
+        return this.read()
+        .then(notes => {
+            let parseNotes;
+            try {
+                parseNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parseNotes = [];
+            }
+            return parseNotes;
+            
+        })
+    }
+    
+
+}
